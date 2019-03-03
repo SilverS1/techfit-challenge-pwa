d9ca8689ef6ae4b8e4ce71045b64fbbb7bf2e798
silvers1 <sarahlecornec123@gmail.com>
2019-03-03 04:17:47 -0800

    header and count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Week from './components/Week';
+import Header from './components/Header';
 
 class App extends Component {
+
+	state = {
+		dateStart: '01/03/2019',
+		dateEnd: '31/03/2019',
+		challengers: [
+			{id: 1, name: 'Jack'},
+			{id: 1, name: 'Mark'},
+			{id: 1, name: 'Sarah'},
+			{id: 1, name: 'Jamie'},
+			{id: 1, name: 'Fabrice'},
+			{id: 1, name: 'Dan'},
+			{id: 1, name: 'Oriane'},
+		]
+	}
   
   render() {
     return (
       <div className="App">
-        <Week></Week>
+      
+      	<Header />
+      	{this.state.challengers.map((challenger, id) => {
+      		return (<Fragment>
+      			<h1>{challenger.name}</h1>
+      				<Week dateStart={this.state.dateStart}
+        	  			  dateEnd={this.state.dateEnd}></Week>
+        	  		</Fragment>)
+
+      	})}
+        
       </div>
     );
   }
--- src/components/Week.js
+++ src/components/Week.js
@@ -7,20 +7,20 @@ class Week extends Component {
 	constructor() {
 		super()
 		uniqueId.enableUniqueIds(this)
+		window.techfit = this.state;
 	}
 
 	state = {
-		name: 'Sarah',
 		days: [
-			{id: 1, date:'01/02/2019', calories: 0},
-			{id: 2, date:'02/02/2019', calories: 0},
-			{id: 3, date:'03/02/2019', calories: 0},
-			{id: 4, date:'04/02/2019', calories: 0},
-			{id: 5, date:'05/02/2019', calories: 0},
-			{id: 6, date:'06/02/2019', calories: 0},
-			{id: 7, date:'07/02/2019', calories: 0},		
+			{id: 1, date:'01/02/2019', calories: 0, exercise: ''},
+			{id: 2, date:'02/02/2019', calories: 0, exercise: ''},
+			{id: 3, date:'03/02/2019', calories: 0, exercise: ''},
+			{id: 4, date:'04/02/2019', calories: 0, exercise: ''},
+			{id: 5, date:'05/02/2019', calories: 0, exercise: ''},
+			{id: 6, date:'06/02/2019', calories: 0, exercise: ''},
+			{id: 7, date:'07/02/2019', calories: 0, exercise: ''},		
 		],
-		totalCalories: 0
+		totalCalories: 0,
 	}
 
 	updateCalories = (id, calorieCount) => {
@@ -33,31 +33,49 @@ class Week extends Component {
 			return (day.id === id);
 		})
 
-		const day = Object.assign([], this.state.days[index]);
+		const day = Object.assign({}, this.state.days[index]);		
 		day.calories = Number(calorieCount.target.value);
 
 		const days = Object.assign([], this.state.days);
 		days[index] = day;
 
+		this.setState({days:days});
+
 		let calories = 0;
-		this.state.days.map((day, index) => {
-			calories += day.calories;
+		days.map((singleDay, index) => {
+			calories += singleDay.calories;
+		});
+
+		this.setState({totalCalories: calories});
+
+	};
+
+	updateExercise = (id, exercise) => {
+
+		if (exercise.target.value.length === 0) {
+			return;
+		}
+
+		const index = this.state.days.findIndex((day) => {
+			return (day.id === id);
 		})
 
-		this.setState({days:days, totalCalories: calories})
+		const day = Object.assign({}, this.state.days[index]);		
+		day.exercise = exercise.target.value;
 
-	}
+		const days = Object.assign([], this.state.days);
+		days[index] = day;
 
-	totalCalories = () => {
-		return(<span>{this.state.totalCalories}</span>)
-	}
+		this.setState({days:days});
+
+	};
 
 	render() {
 
 		return (
 
 			<div>
-				<h1>{this.state.name}</h1>
+			<h1>Total: <span>{this.state.totalCalories}</span></h1>
 				{
 		            this.state.days.map((day, index) => {
 		              return (
@@ -65,16 +83,19 @@ class Week extends Component {
 				              	<Day
 				                date={day.date}
 				                key={index} ></Day>
-				                <input 
+				                Calories: <input 
 				                	onChange={this.updateCalories.bind(this, day.id)} 
-				                	type="text"
-				                	value={day.calories} />
+				                	type="text"/>
+				                Exercise: <input
+				                	onChange={this.updateExercise.bind(this, day.id)}  
+				                	type="text"/>
 			                </div>
 			                )
 			            })
 		          }
 
-		        <h1>Total: {this.totalCalories()}</h1>
+
+		        
 
 			</div>
 
